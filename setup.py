4af76d79a502f9f84f81616e074bd7398d67324b
bsobhani <bsobhani@bnl.gov>
2020-10-05 11:25:02 -0400

    committing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'test_bsobhani',         # How you named your package folder (MyLib)
   packages = ['test_bsobhani'],   # Chose the same as "name"
-  version = '0.1.2',      # Start with a small number and increase it with every change you make
+  version = '0.1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'YOUR NAME',                   # Type in your name
